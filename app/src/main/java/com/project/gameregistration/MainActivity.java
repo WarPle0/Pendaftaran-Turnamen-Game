dcf3b5ec130f52f2cfffe9fae0c5cec23b4e87d5
LuthfiIsmail <luthfiismailv.siswa@smkm4bdg.sch.id>
2021-10-06 20:03:30 +0700

    Revisi seekbar dan pemberian command (Finished)

--- app/src/main/java/com/project/gameregistration/MainActivity.java
+++ app/src/main/java/com/project/gameregistration/MainActivity.java
@@ -15,68 +15,33 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView txtNamaLengkap;
-    private TextView txtNickname;
-    private TextView txtEmail;
-    private TextView txtDomisili;
-    private SeekBar seekBar;
-    private TextView txtRating;
-    private String nilaiRating;
-    private Button btnDaftar;
-//    private RadioGroup radioGroup;
-//    private RadioButton mlbb, lolwr, ff, pubgm, codm, valo;
-//    private String rButtonGames = "";
-//    private CheckBox cb1, cb2, cb3, cb4, cb5, cb6;
-//    private String pilihanCheckBox = "";
+    private TextView txtNamaLengkap; //atribut namalengkap
+    private TextView txtNickname; //atribut nickname
+    private TextView txtEmail; //atribut email
+    private TextView txtDomisili; //atribut domisili
+    private SeekBar seekBar; //atribut seekbar
+    private TextView txtRating; //atribut text rating
+    private String nilaiRating; //atribut nilai rating
+    private Button btnDaftar; //atribut button
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtNamaLengkap = findViewById(R.id.input_namalengkap);
-        txtNickname = findViewById(R.id.input_nickname);
-        txtEmail = findViewById(R.id.input_email);
-        txtDomisili = findViewById(R.id.input_domisili);
-        seekBar = findViewById(R.id.seekbar);
-        txtRating = findViewById(R.id.rating);
-        btnDaftar = findViewById(R.id.daftar);
-//        radioGroup = findViewById(R.id.rgturnamen);
-//        mlbb = findViewById(R.id.mlbb);
-//        lolwr = findViewById(R.id.lolwr);
-//        ff = findViewById(R.id.freefire);
-//        pubgm = findViewById(R.id.pubgm);
-//        codm = findViewById(R.id.codm);
-//        valo = findViewById(R.id.valo);
-//        cb1 = findViewById(R.id.instagram);
-//        cb2 = findViewById(R.id.discord);
-//        cb3 = findViewById(R.id.twitch);
-//        cb4 = findViewById(R.id.youtube);
-//        cb5 = findViewById(R.id.nimotv);
-//        cb6 = findViewById(R.id.lainnya);
+        txtNamaLengkap = findViewById(R.id.input_namalengkap); //inisiasi nama lengkap
+        txtNickname = findViewById(R.id.input_nickname); //inisiasi nickname
+        txtEmail = findViewById(R.id.input_email); //inisiasi email
+        txtDomisili = findViewById(R.id.input_domisili); //inisiasi domisili
+        seekBar = findViewById(R.id.seekbar); //inisiasi seekbar
+        txtRating = findViewById(R.id.rating); //inisiasi rating
+        btnDaftar = findViewById(R.id.daftar); //inisiasi button daftar/submit
 
-//        if (mlbb.isChecked()) {
-//            rButtonGames += "Mobile Legends";
-//        } if (lolwr.isChecked()) {
-//            rButtonGames += "League of Legends";
-//        } if (ff.isChecked()) {
-//            rButtonGames += "Free Fire";
-//        } if (pubgm.isChecked()) {
-//            rButtonGames += "PlayerUnknown's BattleGround Mobile";
-//        } if (codm.isChecked()) {
-//            rButtonGames += "Call of Duty Mobile";
-//        } if (valo.isChecked()) {
-//            rButtonGames += "Valorant";
-//        }
-
-//        if (cb1.isChecked())
-//            pilihanCheckBox += "\n" + cb1.getText().toString();
-
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() { //method listener untuk seekbar
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                nilaiRating = String.valueOf(i);
-                txtRating.setText("Beri nilai tim kami : " + nilaiRating);
+                nilaiRating = String.valueOf(i); //inisiasi value/nilai rating
+                txtRating.setText("Beri nilai tim kami : " + nilaiRating); //method set dan get
             }
 
             @Override
@@ -90,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        btnDaftar.setOnClickListener(new View.OnClickListener() {
+        btnDaftar.setOnClickListener(new View.OnClickListener() { //method button
             @Override
             public void onClick(View view) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
@@ -98,31 +63,29 @@ public class MainActivity extends AppCompatActivity {
                 builder.setTitle("Daftarkan");
                 builder.setMessage(
                         "Apakah anda sudah yakin dengan data anda ?\n\n" +
-                        "Nama Lengkap : \n" + txtNamaLengkap.getText() + "\n\n" +
-                        "Nickname : \n" + txtNickname.getText() + "\n\n" +
-                        "Email : \n" + txtEmail.getText() + "\n\n" +
-                        "Domisili : \n" + txtDomisili.getText() + "\n\n" +
-//                        "Turnamen Game yang diikuti : \n" + radioGroup.getCheckedRadioButtonId() + "\n\n" +
-//                        "Sumber info : \n" + pilihanCheckBox + "\n\n" +
+                        "Nama Lengkap : \n" + txtNamaLengkap.getText() + "\n\n" + //method get
+                        "Nickname : \n" + txtNickname.getText() + "\n\n" + //method get
+                        "Email : \n" + txtEmail.getText() + "\n\n" + //method get
+                        "Domisili : \n" + txtDomisili.getText() + "\n\n" + //method get
                         "Rating : \n" + nilaiRating + ""
                 );
 
-                builder.setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+                builder.setPositiveButton("Ya", new DialogInterface.OnClickListener() { //method button positive desicion
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
                         Toast.makeText(getApplicationContext(), "Data anda berhasil terdaftarkan !", Toast.LENGTH_SHORT).show();
                     }
                 });
 
-                builder.setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+                builder.setNegativeButton("Tidak", new DialogInterface.OnClickListener() { //method button negative desicion
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
 
                     }
                 });
 
-                AlertDialog alertDialog = builder.create();
-                alertDialog.show();
+                AlertDialog alertDialog = builder.create(); //method get alert dan create alert
+                alertDialog.show(); //to show alert
             }
         });
     }
