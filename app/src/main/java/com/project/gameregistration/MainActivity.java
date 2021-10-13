77cc2afad8b16eee4f5e2b35ce24d51f921edf31
Ramdan Rohendi <ramdanrohendi@gmail.com>
2021-10-13 18:54:24 +0700

    Penambahan fungsi intent putExtra

--- app/src/main/java/com/project/gameregistration/MainActivity.java
+++ app/src/main/java/com/project/gameregistration/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,6 +20,14 @@ public class MainActivity extends AppCompatActivity {
     private TextView txtNickname; //atribut nickname
     private TextView txtEmail; //atribut email
     private TextView txtDomisili; //atribut domisili
+    private RadioGroup rgTurnamen; //atribut Radio Grup Turnamen
+    private RadioButton rbMlbb; //atribut MLBB
+    private RadioButton rbLolwr; //atribut LOL WR
+    private RadioButton rbFf; //atribut FF
+    private RadioButton rbPubgm; //atribut PUBGM
+    private RadioButton rbCodm; //atribut CODM
+    private RadioButton rbValorant; //atribut Valorant
+    private String turnamen; //atribut turnamen
     private SeekBar seekBar; //atribut seekbar
     private TextView txtRating; //atribut text rating
     private String nilaiRating; //atribut nilai rating
@@ -29,18 +38,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtNamaLengkap = findViewById(R.id.input_namalengkap); //inisiasi nama lengkap
-        txtNickname = findViewById(R.id.input_nickname); //inisiasi nickname
-        txtEmail = findViewById(R.id.input_email); //inisiasi email
-        txtDomisili = findViewById(R.id.input_domisili); //inisiasi domisili
-        seekBar = findViewById(R.id.seekbar); //inisiasi seekbar
-        txtRating = findViewById(R.id.rating); //inisiasi rating
-        btnDaftar = findViewById(R.id.daftar); //inisiasi button daftar/submit
+        txtNamaLengkap = findViewById(R.id.input_namalengkap); //inisialisasi nama lengkap
+        txtNickname = findViewById(R.id.input_nickname); //inisialisasi nickname
+        txtEmail = findViewById(R.id.input_email); //inisialisasi email
+        txtDomisili = findViewById(R.id.input_domisili); //inisialisasi domisili
+        rgTurnamen = findViewById(R.id.rgturnamen); //inisialisasi Radio Group Turnamen
+        rbMlbb = findViewById(R.id.mlbb); //inisialisasi Radio Button MLBB
+        rbLolwr = findViewById(R.id.lolwr); //inisialisasi Radio Button LOL WR
+        rbFf = findViewById(R.id.freefire); //inisialisasi Radio Button FF
+        rbPubgm = findViewById(R.id.pubgm); //inisialisasi Radio Button PUBGM
+        rbCodm = findViewById(R.id.codm); //inisialisasi Radio Button CODM
+        rbValorant = findViewById(R.id.valo); //inisialisasi Radio Button Valorant
+        seekBar = findViewById(R.id.seekbar); //inisialisasi seekbar
+        txtRating = findViewById(R.id.rating); //inisialisasi rating
+        btnDaftar = findViewById(R.id.daftar); //inisialisasi button daftar/submit
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() { //method listener untuk seekbar
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                nilaiRating = String.valueOf(i); //inisiasi value/nilai rating
+                nilaiRating = String.valueOf(i); //inisialisasi value/nilai rating
                 txtRating.setText("Beri nilai tim kami : " + nilaiRating); //method set dan get
             }
 
@@ -58,15 +74,18 @@ public class MainActivity extends AppCompatActivity {
         btnDaftar.setOnClickListener(new View.OnClickListener() { //method button
             @Override
             public void onClick(View view) {
+                turnamen = getTurnamenSelected();
+
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
                 builder.setIcon(R.drawable.warning);
                 builder.setTitle("Daftarkan");
                 builder.setMessage(
                         "Apakah anda sudah yakin dengan data anda ?\n\n" +
-                        "Nama Lengkap : \n" + txtNamaLengkap.getText() + "\n\n" + //method get
-                        "Nickname : \n" + txtNickname.getText() + "\n\n" + //method get
-                        "Email : \n" + txtEmail.getText() + "\n\n" + //method get
-                        "Domisili : \n" + txtDomisili.getText() + "\n\n" + //method get
+                        "Nama Lengkap : \n" + txtNamaLengkap.getText().toString() + "\n\n" +
+                        "Nickname : \n" + txtNickname.getText().toString() + "\n\n" +
+                        "Email : \n" + txtEmail.getText().toString() + "\n\n" +
+                        "Domisili : \n" + txtDomisili.getText().toString() + "\n\n" +
+                        "Turnament : \n" + turnamen + "\n\n" +
                         "Rating : \n" + nilaiRating + ""
                 );
 
@@ -74,6 +93,16 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
                         Toast.makeText(getApplicationContext(), "Data anda berhasil terdaftarkan !", Toast.LENGTH_SHORT).show();
+
+                        Intent layout2 = new Intent(MainActivity.this, MainActivity2.class);
+                        layout2.putExtra("namaLengkap", txtNamaLengkap.getText().toString());
+                        layout2.putExtra("nickname", txtNickname.getText().toString());
+                        layout2.putExtra("email", txtEmail.getText().toString());
+                        layout2.putExtra("domisili", txtDomisili.getText().toString());
+                        layout2.putExtra("turnament", turnamen);
+                        layout2.putExtra("rating", nilaiRating);
+                        startActivity(layout2);
+                        finish();
                     }
                 });
 
@@ -88,5 +117,26 @@ public class MainActivity extends AppCompatActivity {
                 alertDialog.show(); //to show alert
             }
         });
+    }
+
+    private String getTurnamenSelected() {
+        String turnamen = "";
+        int selectedId = rgTurnamen.getCheckedRadioButtonId();
+
+        if (selectedId == rbMlbb.getId()) {
+            turnamen = "Mobile Legend Bang Bang";
+        } else if (selectedId == rbLolwr.getId()) {
+            turnamen = "League of Legends Wild Rift";
+        } else if (selectedId == rbFf.getId()) {
+            turnamen = "Free Fire";
+        } else if (selectedId == rbPubgm.getId()) {
+            turnamen = "PUBG Mobile";
+        } else if (selectedId == rbCodm.getId()) {
+            turnamen = "Call of Duty Mobile";
+        } else if (selectedId == rbValorant.getId()) {
+            turnamen = "Valorant";
+        }
+
+        return turnamen;
     }
 }
--- app/src/main/java/com/project/gameregistration/MainActivity2.java
+++ app/src/main/java/com/project/gameregistration/MainActivity2.java
@@ -3,12 +3,45 @@ package com.project.gameregistration;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
+    private TextView txtNamaLengkap;
+    private TextView txtNickname;
+    private TextView txtEmail;
+    private TextView txtDomisili;
+    private TextView txtTurnamen;
+    private TextView txtRating;
+    private String namaLengkap;
+    private String nickname;
+    private String email;
+    private String domisili;
+    private String turnamen;
+    private String rating;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        namaLengkap = getIntent().getExtras().getString("namaLengkap");
+        nickname = getIntent().getExtras().getString("nickname");
+        email = getIntent().getExtras().getString("email");
+        domisili = getIntent().getExtras().getString("domisili");
+        turnamen = getIntent().getExtras().getString("turnament");
+        rating = getIntent().getExtras().getString("rating");
+
+        txtNamaLengkap = findViewById(R.id.isi_namalengkap);
+        txtNickname = findViewById(R.id.isi_nickname);
+        txtEmail = findViewById(R.id.isi_email);
+        txtDomisili = findViewById(R.id.isi_domisili);
+        txtTurnamen = findViewById(R.id.isi_turnamen);
+//        txtRating = findViewById(R.id.isi_namalengkap);
+
+        txtNamaLengkap.setText(namaLengkap);
+        txtNickname.setText(nickname);
+        txtEmail.setText(email);
+        txtDomisili.setText(domisili);
+        txtTurnamen.setText(turnamen);
     }
 }
