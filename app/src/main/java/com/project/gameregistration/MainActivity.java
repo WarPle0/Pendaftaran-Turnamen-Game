379002dd94ee3f9612775a656caa27d12d48db2a
Ramdan Rohendi <ramdanrohendi@gmail.com>
2021-10-13 19:44:14 +0700

    Pengecekan Lifecycle dan penambahan komen

--- app/src/main/java/com/project/gameregistration/MainActivity.java
+++ app/src/main/java/com/project/gameregistration/MainActivity.java
@@ -28,6 +28,13 @@ public class MainActivity extends AppCompatActivity {
     private RadioButton rbCodm; //atribut CODM
     private RadioButton rbValorant; //atribut Valorant
     private String turnamen; //atribut turnamen
+    private CheckBox cbInstagram; //atribut Instagram
+    private CheckBox cbDiscord; //atribut Discord
+    private CheckBox cbTwitch; //atribut Twitch
+    private CheckBox cbYoutube; //atribut Youtube
+    private CheckBox cbNimoTV; //atribut NimoTV
+    private CheckBox cbLainnya; //atribut Lainnya
+    private String sumber; //atribut sumber
     private SeekBar seekBar; //atribut seekbar
     private TextView txtRating; //atribut text rating
     private String nilaiRating; //atribut nilai rating
@@ -49,6 +56,12 @@ public class MainActivity extends AppCompatActivity {
         rbPubgm = findViewById(R.id.pubgm); //inisialisasi Radio Button PUBGM
         rbCodm = findViewById(R.id.codm); //inisialisasi Radio Button CODM
         rbValorant = findViewById(R.id.valo); //inisialisasi Radio Button Valorant
+        cbInstagram = findViewById(R.id.instagram); //inisialisasi Checkbox Instagram
+        cbDiscord = findViewById(R.id.discord); //inisialisasi Checkbox Discord
+        cbTwitch = findViewById(R.id.twitch); //inisialisasi Checkbox Twitch
+        cbYoutube = findViewById(R.id.youtube); //inisialisasi Checkbox Youtube
+        cbNimoTV = findViewById(R.id.nimotv); //inisialisasi Checkbox NimoTV
+        cbLainnya = findViewById(R.id.lainnya); //inisialisasi Checkbox Lainnya
         seekBar = findViewById(R.id.seekbar); //inisialisasi seekbar
         txtRating = findViewById(R.id.rating); //inisialisasi rating
         btnDaftar = findViewById(R.id.daftar); //inisialisasi button daftar/submit
@@ -74,11 +87,16 @@ public class MainActivity extends AppCompatActivity {
         btnDaftar.setOnClickListener(new View.OnClickListener() { //method button
             @Override
             public void onClick(View view) {
+                // Mengisi nilai dari hasil return sebuah method
                 turnamen = getTurnamenSelected();
+                sumber = getSumberSelected();
 
+                // Inisialisasi Alert Dialog
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setIcon(R.drawable.warning);
-                builder.setTitle("Daftarkan");
+                builder.setIcon(R.drawable.warning); // Mengeset Icon
+                builder.setTitle("Daftarkan"); // Mengeset Title
+
+                // Mengeset Message
                 builder.setMessage(
                         "Apakah anda sudah yakin dengan data anda ?\n\n" +
                         "Nama Lengkap : \n" + txtNamaLengkap.getText().toString() + "\n\n" +
@@ -86,27 +104,40 @@ public class MainActivity extends AppCompatActivity {
                         "Email : \n" + txtEmail.getText().toString() + "\n\n" +
                         "Domisili : \n" + txtDomisili.getText().toString() + "\n\n" +
                         "Turnament : \n" + turnamen + "\n\n" +
+                        "Sumber : \n" + sumber + "\n\n" +
                         "Rating : \n" + nilaiRating + ""
                 );
 
-                builder.setPositiveButton("Ya", new DialogInterface.OnClickListener() { //method button positive desicion
+                //method button positive desicion
+                builder.setPositiveButton("Ya", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
+                        // Memunculkan Toast
                         Toast.makeText(getApplicationContext(), "Data anda berhasil terdaftarkan !", Toast.LENGTH_SHORT).show();
 
+                        // Inisialisasi Intent untuk berpindah activity
                         Intent layout2 = new Intent(MainActivity.this, MainActivity2.class);
+
+                        // Mengeset data yang akan dikirim ke layout tujuan pada Intent
+                        // Memakai putExtra
                         layout2.putExtra("namaLengkap", txtNamaLengkap.getText().toString());
                         layout2.putExtra("nickname", txtNickname.getText().toString());
                         layout2.putExtra("email", txtEmail.getText().toString());
                         layout2.putExtra("domisili", txtDomisili.getText().toString());
                         layout2.putExtra("turnament", turnamen);
+                        layout2.putExtra("sumber", sumber);
                         layout2.putExtra("rating", nilaiRating);
+
+                        // Memulai Activity tujuan
                         startActivity(layout2);
+
+                        // Mengakhiri Activity ini
                         finish();
                     }
                 });
 
-                builder.setNegativeButton("Tidak", new DialogInterface.OnClickListener() { //method button negative desicion
+                //method button negative desicion
+                builder.setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
 
@@ -119,11 +150,49 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    // Method atau fungsi untuk mengambil data dari Checkbox yang tercheck
+    // Method atau fungsi ini akan mengembalikan data berupa String
+    private String getSumberSelected() {
+        String sumber = "";
+
+        // Jika Checkbox Instagram tercheck
+        if (cbInstagram.isChecked()) {
+            // Maka variabel sumber akan ditambah sama dengan datanya dengan - Instagram
+            // \n disini adalah enter
+            // += berarti atau sama dengan penulisan :
+            // sumber = sumber + "- Instagram\n"
+            sumber += "- Instagram\n";
+        }
+        if (cbDiscord.isChecked()) {
+            sumber += "- Discord\n";
+        }
+        if (cbTwitch.isChecked()) {
+            sumber += "- Twitch\n";
+        }
+        if (cbYoutube.isChecked()) {
+            sumber += "- Youtube\n";
+        }
+        if (cbNimoTV.isChecked()) {
+            sumber += "- NimoTV\n";
+        }
+        if (cbLainnya.isChecked()) {
+            sumber += "- Lainnya\n";
+        }
+
+        return sumber;
+    }
+
+    // Method atau fungsi untuk mengambil data dari Radio Button yang tercheck
+    // Method atau fungsi ini akan mengembalikan data berupa String
     private String getTurnamenSelected() {
         String turnamen = "";
+
+        // Mengambil ID dari Radio Button yang ter check pada Radio Group
         int selectedId = rgTurnamen.getCheckedRadioButtonId();
 
+        // Jika ID yang ter check sama dengan ID Radio button MLBB
         if (selectedId == rbMlbb.getId()) {
+            // Maka Variabel turnamen berisi Mobile Legend Bang Bang
             turnamen = "Mobile Legend Bang Bang";
         } else if (selectedId == rbLolwr.getId()) {
             turnamen = "League of Legends Wild Rift";
--- app/src/main/java/com/project/gameregistration/MainActivity2.java
+++ app/src/main/java/com/project/gameregistration/MainActivity2.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
     private TextView txtNamaLengkap;
@@ -11,12 +12,14 @@ public class MainActivity2 extends AppCompatActivity {
     private TextView txtEmail;
     private TextView txtDomisili;
     private TextView txtTurnamen;
+    private TextView txtSumber;
     private TextView txtRating;
     private String namaLengkap;
     private String nickname;
     private String email;
     private String domisili;
     private String turnamen;
+    private String sumber;
     private String rating;
 
     @Override
@@ -24,11 +27,13 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+        // Ini merupakan cara untuk mengambil data yang dibawa oleh activity sebelumnya
         namaLengkap = getIntent().getExtras().getString("namaLengkap");
         nickname = getIntent().getExtras().getString("nickname");
         email = getIntent().getExtras().getString("email");
         domisili = getIntent().getExtras().getString("domisili");
         turnamen = getIntent().getExtras().getString("turnament");
+        sumber = getIntent().getExtras().getString("sumber");
         rating = getIntent().getExtras().getString("rating");
 
         txtNamaLengkap = findViewById(R.id.isi_namalengkap);
@@ -36,12 +41,47 @@ public class MainActivity2 extends AppCompatActivity {
         txtEmail = findViewById(R.id.isi_email);
         txtDomisili = findViewById(R.id.isi_domisili);
         txtTurnamen = findViewById(R.id.isi_turnamen);
-//        txtRating = findViewById(R.id.isi_namalengkap);
+        txtSumber = findViewById(R.id.isi_sumber);
+        txtRating = findViewById(R.id.isi_rating);
 
+        // Masing-masing TextView di set Text atau isinya sesuai dengan data yang dikirim oleh Activity sebelumnya
         txtNamaLengkap.setText(namaLengkap);
         txtNickname.setText(nickname);
         txtEmail.setText(email);
         txtDomisili.setText(domisili);
         txtTurnamen.setText(turnamen);
+        txtSumber.setText(sumber);
+        txtRating.setText(rating);
+    }
+
+    // Sebuah Fungsi yang dipanggil atau akan tereksekusi saat Activity tampil ke pengguna
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "Menampilkan Activity", Toast.LENGTH_SHORT).show();
+    }
+
+    // Sebuah Fungsi yang dipanggil atau akan tereksekusi saat Activity di hentikan sementara
+    // Bisa di cek ketika anda mengetuk home di HP
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "Menjeda Activity", Toast.LENGTH_SHORT).show();
+    }
+
+    // Sebuah Fungsi yang dipanggil atau akan tereksekusi saat Activity Dimulai kembali ( setelah Pause )
+    // Setelah mengklik home anda bisa membuka aplikasi ini lagi untuk mengecek fungsi ini
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "Memulai Activity Kembali", Toast.LENGTH_SHORT).show();
+    }
+
+    // Sebuah Fungsi yang dipanggil atau akan tereksekusi saat Activity dihancurkan
+    // Atau aplikasi di close atau exit, klik saja tombol kembali atau back pada Hp
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Menghancurkan Activity", Toast.LENGTH_SHORT).show();
     }
 }
